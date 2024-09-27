03f07bf4e66c3eb1528dce35124c56b0606c370f
Dmitriy <planeta977@mail.ru>
2024-09-28 00:10:06 +0300

    feat: added main file

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const DbDataAccess = require('./dao/dbDataAccess');
+const MQTTClient = require('./MQTTClient');
+
+DbDataAccess.getAllData();
+
+const app = express();
+const port = 3000;
+
+const mqttServer = 'mqtt://192.168.1.78:1883';
+new MQTTClient(mqttServer);
+
+app.get('/data', (req, res) => {
+    const { from, to } = req.query;
+
+    if (from && to) {
+        res.send(DbDataAccess.getWaterFlowData(from, to));
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
