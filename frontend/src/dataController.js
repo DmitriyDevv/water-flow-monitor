1f9253bd91e617dd1f5460ed926486c81c7e369b
Dmitriy <planeta977@mail.ru>
2024-10-06 23:41:28 +0300

    feat(frontend): added metrics

--- frontend/src/dataController.js
+++ frontend/src/dataController.js
@@ -20,7 +20,7 @@ class DataController {
             const response = await fetch(
                 `http://${this.serverAddress}:3000/getByDays?from=${from}&to=${to}`
             );
-            
+
             return await response.json();
         } catch (error) {
             console.error('Error fetching data:', error.message);
@@ -28,9 +28,77 @@ class DataController {
         }
     }
 
-    calculateTotalLiters(data) {
-        return data
-            .map((item) => item.totalLiters)
-            .reduce((acc, val) => acc + val, 0);
+    async fetchByDays(from, to) {
+        try {
+            const response = await fetch(
+                `http://${this.serverAddress}:3000/getByDays?from=${from}&to=${to}`
+            );
+
+            return await response.json();
+        } catch (error) {
+            console.error('Error fetching data:', error.message);
+            return [];
+        }
+    }
+
+    async fetchByDays(from, to) {
+        try {
+            const response = await fetch(
+                `http://${this.serverAddress}:3000/getByDays?from=${from}&to=${to}`
+            );
+
+            return await response.json();
+        } catch (error) {
+            console.error('Error fetching data:', error.message);
+            return [];
+        }
+    }
+
+    calculateMetrics(data) {
+        // FIXME: make it possible to set these values from the frontend
+        const waterPrice = 59.8;
+        const waterDisposalPrice = 45.91;
+
+        let accLiters = 0;
+        let minLitersInfo = {
+            liters: Infinity,
+            time: 0,
+        };
+        let maxLitersInfo = {
+            liters: 0,
+            time: 0,
+        };
+
+        data.forEach((item) => {
+            const liters = item.totalLiters;
+            const { timeInterval } = item;
+
+            accLiters += liters;
+
+            if (liters < minLitersInfo.liters) {
+                Object.assign(minLitersInfo, {
+                    liters,
+                    time: timeInterval,
+                });
+            }
+
+            if (liters > maxLitersInfo.liters) {
+                Object.assign(maxLitersInfo, {
+                    liters,
+                    time: timeInterval,
+                });
+            }
+        });
+
+        const averLiters = accLiters / data.length;
+        const price = (accLiters / 1000) * (waterPrice + waterDisposalPrice);
+
+        return {
+            accLiters,
+            averLiters,
+            minLitersInfo,
+            maxLitersInfo,
+            price,
+        };
     }
 }
