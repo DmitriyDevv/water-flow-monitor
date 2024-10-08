61ac04a8b887ca9060086eef914bfeae849e7b65
dmitriy <planeta977@mail.ru>
2024-10-08 17:07:46 +0300

    fix(backend): fixed getDateTime

--- backend/dao/dbDataAccess.js
+++ backend/dao/dbDataAccess.js
@@ -76,17 +76,12 @@ class DbDataAccess {
 
     static getDateTime() {
         const date = new Date();
-        
-        return date.toLocaleString('ru-RU', {
-            timeZone: 'Europe/Moscow',
-            year: 'numeric',
-            month: '2-digit',
-            day: '2-digit',
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit',
-            hour12: false
-        }).replace(',', '');
+        const localTime = new Date(date.toLocaleString("en-US", { timeZone: "Europe/Moscow" }));
+
+        return localTime
+        .toISOString()
+        .slice(0, 19)
+        .replace('T', ' ');
     }
 }
 
