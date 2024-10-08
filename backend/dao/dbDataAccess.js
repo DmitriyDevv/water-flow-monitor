80ce8f19af161136c51fa8c70742680afd78c764
dmitriy <planeta977@mail.ru>
2024-10-08 15:10:33 +0300

    fix(backand): update method to get local time

--- backend/dao/dbDataAccess.js
+++ backend/dao/dbDataAccess.js
@@ -76,12 +76,17 @@ class DbDataAccess {
 
     static getDateTime() {
         const date = new Date();
-        const offset = date.getTimezoneOffset() * 60000;
-
-        return new Date(date.getTime() - offset)
-            .toISOString()
-            .slice(0, 19)
-            .replace('T', ' ');
+        
+        return date.toLocaleString('ru-RU', {
+            timeZone: 'Europe/Moscow',
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false
+        }).replace(',', '');
     }
 }
 
