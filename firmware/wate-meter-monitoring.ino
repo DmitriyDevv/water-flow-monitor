340e971e5ea09e6b718f095a0bfa8d3bad66b63c
dmitriy <planeta977@mail.ru>
2024-10-08 15:12:50 +0300

    feat(firmware): add OTA firmware update capability

--- firmware/wate-meter-monitoring.ino
+++ firmware/wate-meter-monitoring.ino
@@ -1,12 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoOTA.h>
 
 const int pin = 5; // D1
 const int debounceDelay = 100;
 int pinState = 0;
 int currentPinState = 0;
 unsigned long lastDebounceTime = 0;
-const char* mqttServer = "192.168.1.78";
+const char* mqttServer = "192.168.1.36";
 const char* topicName = "esp8266/flow"; 
 
 const char* ssid = "Keenetic-2.4";
@@ -35,7 +36,7 @@ void mqttReconnect() {
         Serial.print("Attempting MQTT connection... ");
         client.setServer(mqttServer, 1883);
 
-        if (client.connect("ESP32Client")) {
+        if (client.connect("ESP8266Client")) {
             Serial.println("Connected");
         } else {
             Serial.print("Connect error: ");
@@ -62,13 +63,54 @@ void sendMqttMessage() {
     } 
 }
 
+void setupOTA() {
+    ArduinoOTA.setHostname("ESP8266-FlowMeter");
+
+    ArduinoOTA.onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH) {
+            type = "sketch";
+        } else { // U_SPIFFS
+            type = "filesystem";
+        }
+        Serial.println("Start updating " + type);
+    });
+
+    ArduinoOTA.onEnd([]() {
+        Serial.println("\nEnd");
+    });
+
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) {
+            Serial.println("Auth Failed");
+        } else if (error == OTA_BEGIN_ERROR) {
+            Serial.println("Begin Failed");
+        } else if (error == OTA_CONNECT_ERROR) {
+            Serial.println("Connect Failed");
+        } else if (error == OTA_RECEIVE_ERROR) {
+            Serial.println("Receive Failed");
+        } else if (error == OTA_END_ERROR) {
+            Serial.println("End Failed");
+        }
+    });
+
+    ArduinoOTA.begin();
+}
+
 void setup() {
     Serial.begin(115200);
     setupWifi();
     pinMode(pin, INPUT);
+    setupOTA();
 }
 
 void loop() {
+    ArduinoOTA.handle();
     mqttConnectHandler();
 
     currentPinState = digitalRead(pin);
